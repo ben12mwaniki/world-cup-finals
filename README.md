beb62b61ec376896da52a70245003c6182d7089e
ben12mwaniki <ben12mwaniki@gmail.com>
2024-06-30 11:56:02 -0400

    chore: Update app.js to scrap data and write it to a JSON file, update README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# world-cup-finals
-Simple web scrapping app made using node.js and cheerio 
+# World cup finals
+This app reads FIFA men's finals data from [wikipedia](https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals) and writes it to a JSON file. It uses cheerio to scrap this data.
+
+## Setup and Configuration
+* Fork the repo and run `npm i` to install the dependencies
+* Run `npm run start`
+
+_Disclaimer: Ensure that you have permission to scrap data from a site before proceeding_
+
+
--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-
 const url = 'https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals';
 
+// Get the World Cup finals data from Wikipedia
+//  and write it to a JSON file
 async function getWorldCupFinals() {
   try {
     const finals = [];
@@ -12,8 +13,34 @@ async function getWorldCupFinals() {
     const $ = cheerio.load(data);
     const listItems = $('#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(17) > tbody > tr');
     
+    listItems.each((index, element) => {
+      
+      if (index === 0) {
+        return; // Skip the first iteration as it contains the table headers
+      }
+      const year = $(element).find('th').text();
+      const winner = $(element).find('td:nth-child(2)').text();
+      const score = $(element).find('td:nth-child(3)').text();
+      const runnerUp = $(element).find('td:nth-child(4)').text();
+      const venue = $(element).find('td:nth-child(5)').text();
+      const location = $(element).find('td:nth-child(6)').text();
+ 
+      finals.push({
+        year,
+        winner,
+        score,
+        runnerUp,
+        venue,
+        location
+      });
+    });
     
-    return finals;
+    fs.writeFile('finals.json', JSON.stringify(finals, null, 2), (err) => {
+      if (err) throw err;
+      console.log('Finals data has been written to finals.json');
+    });
+
+    return;
   } catch (error) {
     console.error(error);
   }
@@ -21,6 +48,5 @@ async function getWorldCupFinals() {
 
 getWorldCupFinals();
 
-//#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(17) > tbody > tr:nth-child(1) > th > a
 
 
